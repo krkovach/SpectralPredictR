7f6d2f1e9e72662e010c1cbeb618cec71f142363
krkovach <23402235+krkovach@users.noreply.github.com>
2020-12-17 00:06:51 -0600

    Add files via upload

--- FSSE_Processing.R
+++ FSSE_Processing.R
@@ -13,34 +13,39 @@ require(spectrolab)
 set.seed(112345)
 cores=detectCores()-1
 
-enspecdrive="Z:/neon/NEON_2020_field_data"
+enspecdrive="Z:/townsenduser-rw/projects/ABoVE/ground_data/2019/dry_data/dry_spec"
 
 #----Import Spectra----
-folderlist=unique(sub("/[^/]+$","",list.files(path=enspecdrive,pattern='.sed$', recursive=TRUE, full.names = TRUE)))
+folderlist=unique(sub("/[^/]+$","",list.files(path=enspecdrive,
+                                              pattern='.asd$',
+                                              recursive=TRUE,
+                                              full.names = TRUE)))
 
-cl=makeCluster(cores[1]-1)
+cl=makeCluster(cores)
 registerDoParallel(cl)
 
 finaloutput=foreach(i=folderlist,
                     .combine=rbind,
                     .packages=c('spectrolab'))%dopar%
   {
-    spectra=read_spectra(path=i,format = "sed")
+    spectra=read_spectra(path=i,format = "asd")
     data.frame(spectra,check.names=FALSE)
   };FST=as_spectra(finaloutput,name_idx=1)
 
 stopCluster(cl)
 
+FST=match_sensors(FST,splice_at=c(1001, 1801))
+
 #----Initial Sweep for WR Outliers----
 hist(data.frame(FST)$"X450")
-FSTr=FST[-which(data.frame(FST)$"X450">0.75),]
+FSTr=FST[-which(data.frame(FST)$"X450">0.8),]
 FSTr=FSTr[-which(data.frame(FSTr)$"X1000"<0.05),]
 hist(data.frame(FSTr)$"X450")
 # write.csv(data.frame(FSTr),"NEON_SED_Spec_2020.csv",row.names=FALSE)
 
 
 #----Data Exploration----
-plot(FSTr, lwd = 0.25, lty = 1, col = "grey50", main="Spectra and 95% Quantile")
+plot(FSTr, lwd = 0.25, lty = 1, col = "grey25", main="2019 Fresh and Dry Spectra")
 plot_quantile(FSTr, total_prob = 0.95, col = rgb(1, 0, 0, 0.25), border = FALSE, add = TRUE)
 plot_regions(FSTr, regions = default_spec_regions(), add = TRUE)
 plot_interactive(FSTr)
